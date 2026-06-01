54d571bb116051b6aeebbde29fcb0ae236681704
Hmdan-Ibrahim <hmdanibrahim5@gmail.com>
2026-06-01 15:03:37 +0300

    Prevent the Popover Component from capturing Wheel events
    Removed commented-out code for SelectCom component.

--- src/components/SelectCom.jsx
+++ src/components/SelectCom.jsx
@@ -1,32 +1,3 @@
-// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
-// import { Label } from "./ui/label";
-// import { Input } from "./ui/input";
-
-// const SelectCom = ({ label, value, onValueChange, selectItems, disabled }) => {
-//     console.log("SelectCom", label, selectItems, value);
-//     const labelVal = selectItems.find(item => item.key === value)?.label
-
-//     return (
-//         <div className="min-w-45">
-//             <Label className={"mb-2.5 pr-1.5 text-2xl"}>{label}</Label>
-//             <Input />
-//             <Select
-//                 value={value ?? ""}
-//                 onValueChange={onValueChange}
-//                 disabled={disabled}
-
-//             >
-//                 <SelectTrigger>
-//                     <SelectValue placeholder={!selectItems.length ? `لا يوجد` : `أختر ${label}`}>{labelVal}</SelectValue>
-//                 </SelectTrigger>
-//                 <SelectContent className="overflow-auto max-h-100">
-//                     {selectItems.map((item) => <SelectItem key={item.key} value={item.key}>{item.label}</SelectItem>)}
-//                 </SelectContent>
-//             </Select>
-//         </div >
-//     )
-// }
-
 import * as React from "react"
 import { ChevronsDown, ChevronsUpDown } from "lucide-react"
 import { Button } from "./ui/button"
@@ -69,7 +40,7 @@ const SelectCom = ({ label, value, onValueChange, selectItems, disabled }) => {
                 </PopoverTrigger>
 
                 {!disabled && (
-                    <PopoverContent className="w-full p-0">
+                    <PopoverContent className="w-full p-0" onWheel={(e) => e.stopPropagation()}>
                         <Command>
                             {selectItems?.length > 8 && <CommandInput placeholder={`ابحث عن ${label}...`} />}
 
